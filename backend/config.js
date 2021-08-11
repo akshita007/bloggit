486715a37d23d26f3defe18537ac84b56d4b932f
akshita007 <69304703+akshita007@users.noreply.github.com>
2021-08-11 16:40:05 +0530

    Basic routing for creating post

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 let config={
-    uri :"mongodb+srv://admin:V9L3SUkEnv2DWRF@myblog.iqbwp.mongodb.net/blog?retryWrites=true&w=majority"
+    uri :"mongodb+srv://admin:V9L3SUkEnv2DWRF@myblog.iqbwp.mongodb.net/Post?retryWrites=true&w=majority"
 };
 
 module.exports = config;
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,8 +1,43 @@
 var express = require("express");
+const bodyParser = require('body-parser');
 const Post = require("../models/post");
 const mongoose = require("mongoose");
 const postRouter = express.Router();
 
-postRouter.all("/",(req,res,next)=>{
-    res.send("Welcome to the post router!")
+postRouter.use(bodyParser.json());
+
+postRouter.route("/")
+.get((req,res,next)=>{
+    Post.find({})
+    .then((post)=>{
+        res.statusCode = 200;
+        res.setHeader("Content-Type","application/json");
+        res.json(post);
+    },err =>{
+        next(err);
+    }).catch(err => next(err));
 })
+.post((req,res,next)=>{
+    Post.create(req.body)
+    .then((post)=>{
+        console.log("POST created is",post);
+        res.statusCode = 200;
+        res.setHeader("Content-Type","application/json");
+        res.json(post);
+    },(err)=>next(err))
+    .catch(err => next(err));
+})
+.put((req,res,next)=>{
+    res.statusCode = 403
+    res.end("This operation cannot be performed on /post")
+})
+.delete((req,res,next)=>{
+    Post.remove({})
+    .then(response=>{
+        res.statusCode = 200;
+        res.setHeader("Content-Type","application/json");
+        res.json(response);
+    },err =>next(err))
+    .catch(err =>next(err));
+});
+module.exports = postRouter;
