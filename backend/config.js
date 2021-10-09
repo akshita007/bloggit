bd4c110b8560554aba0ed59734c52ae771295599
akshita007 <69304703+akshita007@users.noreply.github.com>
2021-10-09 23:47:27 +0530

    setting up the env variables

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config();
 let config={
-    uri :"mongodb+srv://admin:V9L3SUkEnv2DWRF@myblog.iqbwp.mongodb.net/Post?retryWrites=true&w=majority",
+    uri :`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@myblog.iqbwp.mongodb.net/Post?retryWrites=true&w=majority`,
     secretKey :"1234-567-8901-2345-6789"
 };
 
