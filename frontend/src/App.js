3da8bf2d5e02688d8e7dbf76d6ea6621c5517c8c
akshita007 <69304703+akshita007@users.noreply.github.com>
2021-10-12 13:10:24 +0530

    basic routing created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
+import {HashRouter as Router, Switch,Route} from "react-router-dom";
+import {Box} from "@material-ui/core";
+import Header from "./components/Header";
+import Home from "./components/Home/Home";
+import PostDetails from "./components/Post/PostDetail";
+import CreatePost from "./components/Post/CreatePost";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-          Welcome
-      </header>
-    </div>
+      <Router>
+        <Header/>
+        <Box style = {{marginTop: 64}}>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path = '/postDetails' component={PostDetails}></Route>
+            <Route path = '/CreatePost' component={CreatePost}></Route>
+          </Switch>
+        </Box>
+      </Router>
   );
 }
 
