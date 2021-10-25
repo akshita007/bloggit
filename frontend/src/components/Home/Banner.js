d0c4c731b6dea793138898b2e0bb40f3e8852002
akshita007 <69304703+akshita007@users.noreply.github.com>
2021-10-25 16:27:45 +0530

    parallax effect added

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import { Box, makeStyles, Typography } from '@material-ui/core';
+import {Parallax} from 'react-parallax';
+import {makeStyles, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
-image:{
-    background:`url(${"https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdvcmtzcGFjZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=60"}) center no-repeat black`,
-    width: '100%',
-    height:'90vh',
-    display:"flex",
-    alignItems:"center",
-    justifyContent:'center',
-    "& > *":{
-        background: 'rgba(255,255,255,0.8)',
-        textAlign:'center',
-        color:"black",
-        fontSize:"70px",
-        fontFamily:"comic sans",
-        fontStyle:"italic"
-    }
+
+text:{
+    
+    background: 'rgba(255,255,255,0.8)',
+    textAlign:'center',
+    color:"black",
+    fontSize:"70px",
+    fontFamily:"comic sans",
+    fontStyle:"italic",
 }
 })
 const Banner=()=> {
     const classes = useStyles();
     return (
-        <Box className={classes.image}>
-            {!localStorage.getItem('token')?(<Typography>Hello! Why don't you login, and enter in the world of blogging!</Typography>):(<Typography>Welcome Back! {localStorage.getItem('user')} </Typography>)}
-        </Box>
+    <Parallax bgImage={"https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdvcmtzcGFjZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=60"} bgImageAlt="alternate" strength={700}>
+        <div style={{ height: '80vh', display:'flex',justifyContent:'center',alignItems:'center' }}>
+            {!localStorage.getItem('token')?(<Typography className={classes.text}>Hello! Why don't you login, and enter in the world of blogging!</Typography>):(<Typography className={classes.text}>Welcome Back! {localStorage.getItem('user')} </Typography>)}
+        </div>
+    </Parallax>
     )
 };
 
